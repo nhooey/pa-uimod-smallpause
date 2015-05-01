94da10d556c77b65fe98cc8223bfbc38fe57377c
Neil Hooey <nhooey@gmail.com>
2015-05-01 14:47:06 -0400

    Changed mod to dynamically set styles with JavaScript instead.
    - Thanks to @ColaColin for the pull request:
      https://github.com/nhooey/pa-uimod-smallpause/pull/1

---
+++ ui/mods/smallpause/smallpause.js
@@ -0,0 +1,24 @@
+console.log("Setting styles for \"Small Pause\" mod...");
+
+$('.div_panel_bar_background').css({
+    'width': '260px',
+    'margin': '20px 0 0 -130px',
+    'position': 'absolute',
+    'top': '20px',
+    'left': '50%'
+});
+
+$('.div_panel_primary_msg').css({
+    'height': '30px',
+    'line-height': '15px',
+    'font-size': '20px',
+    'padding': '8px 0px 0px 0px'
+});
+
+$('input[type="button"]').css({
+    'padding': '4px 36px'
+});
+
+$('.div_bottom_margin_bar').css({
+    'height': '5px'
+});
